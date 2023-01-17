c3eb188e39de8ca4eef0cf3c40c7cbea2216147a
Anna Parissi <anna.parissi@persado.com>
2023-01-17 14:31:30 +0200

    Initial commit

---
+++ src/main/java/com/example/server/controller/TaskController.java
@@ -0,0 +1,24 @@
+package com.example.server.controller;
+
+import com.example.server.service.TaskService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "api/")
+@AllArgsConstructor
+public class TaskController {
+    TaskService taskService;
+
+    @GetMapping(value = "tasks", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getTasks() {
+        return ResponseEntity.ok().body(
+                taskService.getTasks());
+    }
+
+}
+
